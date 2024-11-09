707afb9d8c046c8cddf446335f69f9816ca4504d
ShreyaSanjayAkole <shreyaakole143@gmail.com>
2024-11-09 14:32:08 +0530

    Handle Exception if User Provides Invalid Mood

--- MoodAnalyser.js
+++ MoodAnalyser.js
@@ -4,24 +4,30 @@ class MoodAnalyser {
     }
 
     analyseMood() {
-      const happyKeywords = ["happy", "joy", "any","excited", "pleased", "delighted"];
-      const sadKeywords = ["sad", "unhappy", "upset", "down", "depressed"];
-  
-      const lowerCaseMessage = this.message.toLowerCase();
-  
-      if (happyKeywords.some(word => lowerCaseMessage.includes(word))) {
-        return "Happy";
-      }
-  
-      if (sadKeywords.some(word => lowerCaseMessage.includes(word))) {
-        return "Sad";
-      }
-
-      return "Neutral";
-
-    }
+      try {
+        
+        if (this.message == null || typeof this.message !== "string") {
+              throw new Error("Invalid mood message");
+          }
+        const happyKeywords = ["happy", "joy", "any","excited", "pleased", "delighted"];
+        const sadKeywords = ["sad", "unhappy", "upset", "down", "depressed"];
     
+        const lowerCaseMessage = this.message.toLowerCase();
+    
+        if (happyKeywords.some(word => lowerCaseMessage.includes(word))) {
+          return "Happy";
+        }
+    
+        if (sadKeywords.some(word => lowerCaseMessage.includes(word))) {
+          return "Sad";
+        }
+
+        return "Neutral";
+      }catch (error) {
+        return "Happy";
+    }
   }
+}
 
 if (typeof window !== "undefined") {
     const userMessage = prompt("How are you feeling today?");
